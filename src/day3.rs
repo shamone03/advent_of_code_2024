8737245c9182d0e642450dbf5f9727533be2da6d
Aryah Kannan <aryah.kannan@cartenav.com>
2024-12-03 13:25:21 -0400

    iterators only

--- src/day3.rs
+++ src/day3.rs
@@ -14,26 +14,27 @@ fn part1(input: &str) -> i64 {
 fn part2(input: &str) -> i64 {
     let regex =
         Regex::new(r"(?m)(do\(\)|don't\(\)|mul\((?<num1>[0-9]+),(?<num2>[0-9]+)\))").unwrap();
-    let mut enabled = true;
-    regex
-        .captures_iter(input)
-        .map(|captures| {
-            if let Some(matched) = captures.get(0) {
-                enabled = match matched.as_str() {
-                    "do()" => true,
-                    "don't()" => false,
-                    _ => enabled,
-                };
-            }
-            if let (Some(num1), Some(num2)) = (captures.name("num1"), captures.name("num2")) {
-                if enabled {
-                    return num1.as_str().parse::<i64>().unwrap()
-                        * num2.as_str().parse::<i64>().unwrap();
+
+    let (sum, _) =
+        regex
+            .captures_iter(input)
+            .fold((0, true), |(mut sum, mut enabled), captures| {
+                if let Some(matched) = captures.get(0) {
+                    enabled = match matched.as_str() {
+                        "do()" => true,
+                        "don't()" => false,
+                        _ => enabled,
+                    };
                 }
-            }
-            0
-        })
-        .sum()
+                if let (Some(num1), Some(num2)) = (captures.name("num1"), captures.name("num2")) {
+                    if enabled {
+                        sum += num1.as_str().parse::<i64>().unwrap()
+                            * num2.as_str().parse::<i64>().unwrap();
+                    }
+                }
+                (sum, enabled)
+            });
+    sum
 }
 pub fn answers() -> (i64, i64) {
     let input = include_str!("../input/day3.txt");
